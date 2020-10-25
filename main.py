9da5f08416e12f2c0ba259b4238834749c22c9c0
Josue Lubaki <lubaki@uqtr.ca>
2020-10-25 07:16:01 -0400

    Version_4

--- main.py
+++ main.py
@@ -1,35 +1,125 @@
 import pygame
 from Fonctional import *
-pygame.init()
-
+ 
+pygame.init() 
+  
+# define the RGB value for white, 
+#  green, blue colour . 
+blue = (0, 0, 128) 
 noir = (0, 0 ,0)
 blanc = (255, 255, 255)
 vert =  (0, 255, 0)
 rouge = (255, 0, 0)
-
-Taille =[550, 550]
-fenetre = pygame.display.set_mode(Taille)
-
-#Titre and Icon
-pygame.display.set_caption('TIC TAC TOE')
-icon = pygame.image.load('tic-tac-toe_32.png')
+rouge_sombre = (153,0,0) 
+blue_sombre = (0,51,51)
+orange_sombre = (204,102,0)
+orange_claire = (255,128,0)
+  
+# assigning values to X and Y variable 
+X = 550
+Y = 550
+  
+# create the display surface object 
+# of specific dimension..e(X, Y). 
+fenetre = pygame.display.set_mode((X,Y))
+display_surface = pygame.display.set_mode((X, Y ))
+ 
+# set the pygame window name 
+pygame.display.set_caption('TIC-TAC-TOE')
+icon = pygame.image.load("tic-tac-toe_32.png")
 pygame.display.set_icon(icon)
 
-# initialisation des variables rec qui corespond aux rectangles
-# initialisation de case vide
-case = initialiser_case()
-rec = init_variable(pygame, fenetre, blanc)
+# create a font object. 
+# 1st parameter is the font file 
+# which is present in pygame. 
+# 2nd parameter is size of the font 
+font_titre = pygame.font.Font('freesansbold.ttf', 38)
+font = pygame.font.Font('freesansbold.ttf', 32) 
+font_info = pygame.font.Font('freesansbold.ttf', 24) 
+  
+# create a text suface object, 
+# on which text is drawn on it. 
+text = font_titre.render('TIC - TAC - TOE', True, orange_claire)
+text_2 = font.render('Faites le Choix de votre Symbole', True, orange_sombre)
+
+# create a rectangular object for the 
+# text surface object 
+textRect = text.get_rect() 
+textRect_2 = text_2.get_rect() 
 
 #attention a C de Clock
 horloge = pygame.time.Clock()
+
+# set the center of the rectangular object. 
+textRect.center = (X // 2, Y // 5) 
+textRect_2.center = (X // 2, Y // 2)
+
+fin = False
+choixUser = "Y" 
+
+# LOGIQUE DU MENU DU JEU
+while fin == False: 
+    
+    # completely fill the surface object 
+    # with white color 
+    display_surface.fill(blue_sombre)
+    # Creation de deux cases pour le choix du symbole
+    rec_1 = pygame.draw.rect(display_surface, blanc, (100,300,150,150))
+    rec_2 = pygame.draw.rect(display_surface, blanc, (275,300,150,150))
+
+    # copying the text surface object 
+    # to the display surface object  
+    # at the center coordinate. 
+    display_surface.blit(text, textRect) 
+    display_surface.blit(text_2, textRect_2) 
+  
+    # iterate over the list of Event objects 
+    # that was returned by pygame.event.get() method. 
+    for event in pygame.event.get() : 
+        # if event object type is QUIT 
+        # then quitting the pygame 
+        # and program both. 
+        if event.type == pygame.QUIT : 
+            fin = True
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            print(pos)
+            if rec_1.collidepoint(pos):
+                choixUser = "X"
+            elif rec_2.collidepoint(pos):
+                choixUser = "O"
+            else:
+                choixUser = "X [default]"
+                fin = False
+            # Deplacer les cases de choix vers l'origine (0,0)
+            if choixUser == "X" or choixUser == "O":
+                rec_1 = pygame.draw.rect(display_surface, blanc, (0,0,0,0))
+                rec_2 = pygame.draw.rect(display_surface, blanc, (0,0,0,0))
+                fin = True
+            else:
+                fin = False
+    pygame.display.update()
+
+# CHECKER LE CHOIX DU USER (code ici) ♠
+if fin == True:
+    # Coder ici | Quand tout va bien
+    pass
+if(fin == True and choixUser == "X [default]"):
+    # Coder ici | User veut partir | text de confirmation
+    pass
+
+# Création de la fenetre de Jeu
+fenetre = pygame.display.set_mode((X,Y))
+# Initialisation des variables pour le jeu
+case = initialiser_case()
+rec = init_variable(pygame, fenetre, blanc)
+
 is_running = True
-# is_running correspond à la variable "termine"
 while is_running == True:
-    #pygame.event.get() est un list
     for event in pygame.event.get():
         if ( event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and
         event.key == pygame.K_q)):
-            # Quit
+            # Quitter le jeu sur QUIT [X] ou la touche "Q"
             is_running = False
             
         #redémarrage du jeu
@@ -37,10 +127,15 @@ while is_running == True:
             # Reinitialiser les rectangles et les cases disponibles
             rec = init_variable(pygame,fenetre,blanc)
             case = initialiser_case()
-                
-    # LOGIQUE DU MENU DU JEU
-        """ ICI """
 
+        # Affichage du choix de l'Utilisateur
+        texte = font_info.render('Votre choix est '+ choixUser,True,blanc,(0,0,0)) 
+        # create a rectangular object for the 
+        # text surface object 
+        textRect = texte.get_rect()
+        # copying the text surface object 
+        # to the display surface object 
+        fenetre.blit(texte, textRect) 
 
     #LOGIQUE DU JEU
         if event.type == pygame.MOUSEBUTTONUP:
@@ -146,10 +241,8 @@ while is_running == True:
                     pygame.draw.ellipse(fenetre, vert, [385,385,130,130], 30)
                     rec[0] = True
                 case[9] = False
-    
-    #dessiner
 
     pygame.display.flip()
     #20 images par seconde, 10 moin vite que 20        
-    horloge.tick(20)
+    horloge.tick()
 pygame.quit()
